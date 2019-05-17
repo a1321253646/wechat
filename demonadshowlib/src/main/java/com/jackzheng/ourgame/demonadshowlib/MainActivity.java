b3066584465ac7223be19f4ae7f4656fe14cdf8a
1321253646@qq.com <1321253646@qq.com>
2019-05-17 19:18:31 +0800

    fuck

---
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -0,0 +1,43 @@
+package com.jackzheng.ourgame.demonadshowlib;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.umeng.analytics.MobclickAgent;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("jackzhng","onResume===========================");
+        MobclickAgent.onResume(this);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("jackzhng","onPause===========================");
+        MobclickAgent.onPause(this);
+    }
+
+    public String showTaptap(String str){
+  /*      Log.d("jackzhng","playAd===========================");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse("taptap://taptap.com/app?app_id=150760&source=outer"));
+        if (intent != null && intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }else{
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("http://d.taptap.com/latest?app_id=150760"));
+            startActivity(intent);
+        }*/
+        return "";
+    }
+
+}
