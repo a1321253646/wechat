c8413e70d0ef2c12330f90a1c515d0a099dea550
1321253646@qq.com <1321253646@qq.com>
2019-05-21 02:57:50 +0800

    时好时不行

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -1,18 +1,27 @@
 package com.jackzheng.ourgame.demonadshowlib;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.miui.zeus.mimo.sdk.MimoSdk;
 import com.miui.zeus.mimo.sdk.ad.AdWorkerFactory;
 import com.miui.zeus.mimo.sdk.ad.IAdWorker;
 import com.miui.zeus.mimo.sdk.listener.MimoAdListener;
 import com.umeng.analytics.MobclickAgent;
+import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 import com.xiaomi.ad.common.pojo.AdType;
 
@@ -21,13 +30,44 @@ import miui.os.zeus.Build;
 public class MainActivity extends UnityPlayerActivity {
 
     private IAdWorker mAdWorker;
+    IAdWorker mBannerAd;
+    IAdWorker mSplashAd;
     public static final String TAG = "jackzhng";
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+
         getmAdWorker();
+        mHandler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if(msg.what == 1){
+                    playInerAdDeal();
+                }else if(msg.what == 2){
+                    mHandler.removeMessages(2);
+                    if(isAdInit("")){
+                        startShowBannerDeal();
+                    }else{
+                        mHandler.sendEmptyMessageDelayed(2,500);
+                    }
+
+                }else if(msg.what == 3){
+                    playSplashAdDeal();
+                }else if(msg.what == 4){
+                    mHandler.removeMessages(4);
+                    if(isAdInit("")){
+                        playSplashAdDeal();
+                    }else{
+                        mHandler.sendEmptyMessageDelayed(4,500);
+                    }
+                }
+            }
+        };
     }
 
+
+    private Handler mHandler= null;
 
     private IAdWorker getmAdWorker(){
         if(mAdWorker != null){
@@ -37,19 +77,30 @@ public class MainActivity extends UnityPlayerActivity {
                 mAdWorker = AdWorkerFactory.getAdWorker(this, (ViewGroup) getWindow().getDecorView(), new MimoAdListener() {
                     @Override
                     public void onAdPresent() {
+
                         Log.e(TAG, "onAdPresent");
+                        startGameOrPause(false);
                     }
 
                     @Override
                     public void onAdClick() {
                         Log.e(TAG, "onAdClick");
+                        try {
+                            startGameOrPause(true);
+                            getmAdWorker().recycle();
+                            getmAdWorker().load(MainApplication.INSERT_ID);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
 
                     @Override
                     public void onAdDismissed() {
                         Log.e(TAG, "onAdDismissed");
                         try {
-                            getmAdWorker().load("b90939fae6df77c0b5989fbac3ef810a");
+                            startGameOrPause(true);
+                            getmAdWorker().recycle();
+                            getmAdWorker().load(MainApplication.INSERT_ID);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -85,7 +136,7 @@ public class MainActivity extends UnityPlayerActivity {
         super.onResume();
         Log.d("jackzhng","onResume===========================");
         MobclickAgent.onResume(this);
-
+    //    mHandler.sendEmptyMessageDelayed(4,500);
     }
 
 
@@ -109,7 +160,7 @@ public class MainActivity extends UnityPlayerActivity {
             boolean ready =  getmAdWorker().isReady();
             Log.d("jackzhng","isInserAdReady==========================="+ready);
             if(!ready){
-                getmAdWorker().load("b90939fae6df77c0b5989fbac3ef810a");
+                getmAdWorker().load(MainApplication.INSERT_ID);
             }
             return ready;
         } catch (Exception e) {
@@ -120,16 +171,95 @@ public class MainActivity extends UnityPlayerActivity {
 
     public boolean playInerAd(String str){
         Log.d("jackzhng","playInerAd===========================");
+        mHandler.sendEmptyMessage(1);
+
+        return true;
+    }
+    public boolean playSplashAd(String str){
+        Log.d("jackzhng","playSplashAd===========================");
+        mHandler.sendEmptyMessage(3);
+        return true;
+    }
+    ViewGroup mSplashView;
+    private void playSplashAdDeal( ){
+        mSplashView = new FrameLayout(this) ;
+        Log.d("jackzhng","playSplashAdDeal===========================");
+        String[]  strs = mBannerPoint.split(",");
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        float density = dm.density;
+        WindowManager mWindowManager = (WindowManager) MainActivity.this.getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams mWmParams = new WindowManager.LayoutParams();
+        mWmParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        mWmParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+        mWmParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        mWmParams.gravity = Gravity.BOTTOM|Gravity.CENTER;
+        mWindowManager.addView(mSplashView, mWmParams);
+
+        Log.d("jackzhng","playInerAd===========================");
+        try {
+            mSplashAd = AdWorkerFactory.getAdWorker(this, mSplashView, new MimoAdListener() {
+                @Override
+                public void onAdPresent() {
+                    // 开屏广告展示
+                    Log.d(TAG, "onAdPresent");
+                }
+
+                @Override
+                public void onAdClick() {
+                    //用户点击了开屏广告
+                    Log.d(TAG, "onAdClick");
+                    mSplashView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAdDismissed() {
+                    //这个方法被调用时，表示从开屏广告消失。
+                    Log.d(TAG, "onAdDismissed");
+                    mSplashView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAdFailed(String s) {
+                    Log.e(TAG, "ad fail message : " + s);
+                    mSplashView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAdLoaded(int size) {
+                    //do nothing
+                }
+
+                @Override
+                public void onStimulateSuccess() {
+                }
+            }, AdType.AD_SPLASH);
+
+            mSplashAd.loadAndShow(MainApplication.SPLASH_ID);
+        } catch (Exception e) {
+            e.printStackTrace();
+            mSplashView.setVisibility(View.GONE);
+        }
+    }
+
+
+    private void playInerAdDeal(){
         try {
             getmAdWorker().show();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return true;
     }
 
     public void startGameOrPause(boolean isStart){
-
+        String action = "";
+        if(isStart){
+            action = "start";
+        }else{
+            action = "pause";
+        }
+        UnityPlayer.UnitySendMessage("Main Camera", "startOrPause", action);
     }
     public String showTaptap(String str){
   /*      Log.d("jackzhng","playAd===========================");
@@ -147,11 +277,109 @@ public class MainActivity extends UnityPlayerActivity {
         return "";
     }
 
+    private boolean isShowBanner = false;
+    private String mBannerPoint = "";
+    public boolean startShowBanner(String point){
+        mBannerPoint = point;
+        mHandler.sendEmptyMessage(2);
+
+        return true;
+    }
+    ViewGroup mBannerView;
+    private void startShowBannerDeal(){
+
+        Log.e(TAG, "startShowBanner");
+        if(isShowBanner){
+            return ;
+        }
+        if(mBannerView == null){
+            mBannerView = new FrameLayout(this) ;
+            String[]  strs = mBannerPoint.split(",");
+            float weight = Float.parseFloat(strs[0]);
+            float y = Float.parseFloat(strs[1]);
+            WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+            DisplayMetrics dm = new DisplayMetrics();
+            wm.getDefaultDisplay().getMetrics(dm);
+            float density = dm.density;
+            float screenHeight = dm.heightPixels;
+            WindowManager mWindowManager = (WindowManager) MainActivity.this.getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams mWmParams = new WindowManager.LayoutParams();
+            mWmParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+            mWmParams.height =(int)( 54 *density);
+            mWmParams.width = (int)weight;
+            mWmParams.gravity = Gravity.TOP|Gravity.CENTER;
+            mWmParams.y = (int)(screenHeight- y);
+            mWindowManager.addView(mBannerView, mWmParams);
+        }
+
+        if(mBannerAd == null){
+            try {
+                mBannerAd = AdWorkerFactory.getAdWorker(this, mBannerView, new MimoAdListener() {
+                    @Override
+                    public void onAdPresent() {
+                        Log.e(TAG, "startShowBannerDeal onAdPresent");
+                    }
+
+                    @Override
+                    public void onAdClick() {
+                        Log.e(TAG, "startShowBannerDeal onAdClick");
+                     /*   try {
+                            mBannerAd.loadAndShow("6a5cfe092eb49ba667221ea9189e0cd9");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }*/
+                    }
+
+                    @Override
+                    public void onAdDismissed() {
+                        Log.e(TAG, "startShowBannerDeal onAdDismissed");
+                     /*   try {
+                            mBannerAd.loadAndShow("6a5cfe092eb49ba667221ea9189e0cd9");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }*/
+                    }
+
+                    @Override
+                    public void onAdFailed(String s) {
+                        Log.e(TAG, "startShowBannerDeal onAdFailed :"+s);
+                        try {
+                            isShowBanner = false;
+                            mHandler.sendEmptyMessageDelayed(2,500);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onAdLoaded(int size) {
+                        Log.e(TAG, "startShowBannerDeal onAdLoaded");
+                    }
+
+                    @Override
+                    public void onStimulateSuccess() {
+                        Log.e(TAG, "startShowBannerDeal onStimulateSuccess");
+                    }
+                }, AdType.AD_BANNER);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try{
+            mBannerAd.loadAndShow(MainApplication.BANNER_ID);
+            isShowBanner = true;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     protected void onDestroy() {
         try {
             super.onDestroy();
             getmAdWorker().recycle();
+            mBannerAd.recycle();
         } catch (Exception e) {
         }
     }
--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
@@ -15,6 +15,16 @@ public class MainApplication extends Application {
     public static String[] mAppKeyUmeng = {"5cd62f530cafb2cc4c0007df","5cd62f633fc1957973000f46","5cd62f783fc195f2680002ef","5cd62f884ca3570403000919","5cd62f9b570df34c47000fde",
             "5cd62fbc0cafb254a00004ab","5cd62fce570df31d670001d5","5cd62fdd3fc19537dd000660","5cd62fe73fc195f268000303"};
 
+    public static String APP_ID = "2882303761518007399";
+    public static String BANNER_ID = "6a5cfe092eb49ba667221ea9189e0cd9";
+    public static String INSERT_ID = "b90939fae6df77c0b5989fbac3ef810a";
+    public static String SPLASH_ID = "207ef87c481288193cc8be9cb5e98151";
+
+ //    public static String APP_ID = "2882303761518007399";
+ //    public static String BANNER_ID = "6a5cfe092eb49ba667221ea9189e0cd9";
+ //    public static String INSERT_ID = "b90939fae6df77c0b5989fbac3ef810a";
+ //    public static String SPLASH_ID = "207ef87c481288193cc8be9cb5e98151";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -39,7 +49,7 @@ public class MainApplication extends Application {
         android.util.Log.d("jackzhng","mAppKeyUmeng id = "+mAppKeyUmeng[Integer.parseInt(name)]);
         UMConfigure.setLogEnabled(true);
         UMConfigure.init(this, mAppKeyUmeng[Integer.parseInt(name)], "Umeng", UMConfigure.DEVICE_TYPE_PHONE,null);
-        MimoSdk.init(this, "appid:2882303761518007399", "fake_app_key", "fake_app_token");
+        MimoSdk.init(this, APP_ID, "fake_app_key", "fake_app_token");
 
         MimoSdk.setDebugOn();
 
