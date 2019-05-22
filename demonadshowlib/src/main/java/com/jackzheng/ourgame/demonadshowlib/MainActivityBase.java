8e250c51bd00bc4e8049ab88e4ee81280989709b
1321253646@qq.com <1321253646@qq.com>
2019-05-22 23:30:46 +0800

    添加后台控制

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivityBase.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivityBase.java
@@ -20,6 +20,8 @@ public abstract class MainActivityBase extends UnityPlayerActivity {
     public abstract void startShowBannerDeal();
     public String mBannerPoint = "";
 
+    public static String CHANNEL_VERSION = "";
+
     public void startGameOrPause(boolean isStart){
         if(!isStart && isPause){
             return;
@@ -52,11 +54,11 @@ public abstract class MainActivityBase extends UnityPlayerActivity {
                     }
 
                 }else if(msg.what == 3){
-                    mHandler.removeMessages(4);
+                    mHandler.removeMessages(3);
                     if(isAdInit("")){
                         playSplashAdDeal();
                     }else{
-                        mHandler.sendEmptyMessageDelayed(4,500);
+                        mHandler.sendEmptyMessageDelayed(3,500);
                     }
                 }
             }
