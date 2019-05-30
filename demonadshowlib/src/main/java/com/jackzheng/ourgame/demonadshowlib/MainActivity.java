c11c9cfb21baf4307131dee2a46109d7fcea47c9
1321253646@qq.com <1321253646@qq.com>
2019-05-30 12:20:05 +0800

    添加平台sdk

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -1,6 +1,12 @@
 package com.jackzheng.ourgame.demonadshowlib;
 
 
+import android.os.Bundle;
+
+import com.gionee.game.offlinesdk.floatwindow.AppInfo;
+import com.gionee.game.offlinesdk.floatwindow.GamePlatform;
+import com.gionee.gsp.GnEFloatingBoxPositionModel;
+
 public class MainActivity extends MainActivityBase {
 
 
@@ -27,5 +33,14 @@ public class MainActivity extends MainActivityBase {
     @Override
     public void startShowBannerDeal() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        AppInfo appInfo = new AppInfo();
+        appInfo.setApiKey(MainApplication.API_KEY);// apiKey由开发者后台申请得到
+        appInfo.setPrivateKey(MainApplication.PRIVATE_KEY); //privateKey由开发者后台申请得到
+        GamePlatform.init(this, appInfo, GnEFloatingBoxPositionModel.LEFT_TOP);
     }
 }
--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
@@ -5,6 +5,9 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.text.TextUtils;
 
+import com.gionee.game.offlinesdk.floatwindow.AppInfo;
+import com.gionee.game.offlinesdk.floatwindow.GamePlatform;
+import com.gionee.gsp.GnEFloatingBoxPositionModel;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 
@@ -14,6 +17,9 @@ public class MainApplication extends Application {
 
     public static String[] mAppKeyUmeng = {"5cd62f530cafb2cc4c0007df","5cd62f633fc1957973000f46","5cd62f783fc195f2680002ef","5cd62f884ca3570403000919","5cd62f9b570df34c47000fde",
             "5cd62fbc0cafb254a00004ab","5cd62fce570df31d670001d5","5cd62fdd3fc19537dd000660","5cd62fe73fc195f268000303"};
+
+    public static String API_KEY = "2BD1FE82388240CA9D83A9041A9E9674";
+    public static String PRIVATE_KEY = "CA7137B43B6D4B788A0422530AAE0ABD";
 
     @Override
     public void onCreate() {
