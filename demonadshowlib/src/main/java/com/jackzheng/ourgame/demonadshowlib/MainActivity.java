00b5929130fd4459afb094b84da2d363f2cba5ea
1321253646@qq.com <1321253646@qq.com>
2019-05-30 15:43:27 +0800

    接入广告

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -3,6 +3,11 @@ package com.jackzheng.ourgame.demonadshowlib;
 
 public class MainActivity extends MainActivityBase {
 
+    public static String APP_ID = "1246";
+    public static String BANNER_ID = "4684";
+    public static String INSERT_ID = "4683";
+    public static String SPLASH_ID = "4682";
+
 
     @Override
     public boolean isAdInit(String str) {
