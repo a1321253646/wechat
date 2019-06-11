9486c064548a108de65e5475461ca58f4ecea42a
1321253646@qq.com <1321253646@qq.com>
2019-06-12 01:17:16 +0800

    处理不知道什么

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -1,9 +1,22 @@
 package com.jackzheng.ourgame.demonadshowlib;
 
+import android.util.Log;
+
+import com.jackzheng.ourgame.demonadshowlib.sqlite.SQLDate;
+import com.jackzheng.ourgame.demonadshowlib.sqlite.SqliteControl;
 import com.unity3d.player.UnityPlayerActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends UnityPlayerActivity {
 
+
+    SqliteControl mSqliteControl ;
     @Override
     protected void onResume() {
         super.onResume();
@@ -15,19 +28,240 @@ public class MainActivity extends UnityPlayerActivity {
         super.onPause();
     }
 
+    public void createTable(String sqlName,String tableName){
+        Log.d("mysql","createTable sqlName="+sqlName+" tableName="+tableName);
+        mSqliteControl  = new SqliteControl(MainActivity.this,sqlName,null,1,tableName);
+        Log.d("mysql","createTable end");
+    }
+
+
+    public long getLevel(){
+        Log.d("mysql","getLevel");
+        long level =  mSqliteControl.getLevel();
+        Log.d("mysql","getLevel end="+level+" end");
+        return level;
+
+    }
+    public long getPlayVocation(){
+        Log.d("mysql","getPlayVocation");
+        long vocation =   mSqliteControl.getLevel();
+        Log.d("mysql","getLevel end="+vocation+" end");
+        return vocation;
+    }
+
+    public void deleteGuide(String str){
+        Log.d("mysql","deleteGuide str = "+str);
+        long id = Long.parseLong(str);
+         mSqliteControl.deleteGuide(id);
+        Log.d("mysql","deleteGuide end");
+    }
+
+    public void onUodateInfoByTypeAndId(String str) {
+        Log.d("mysql","onUodateInfoByTypeAndId str = "+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.onUodateInfoByTypeAndId(date);
+        Log.d("mysql","onUodateInfoByTypeAndId end");
+    }
+
+    public void alterTableForIsNetAndIsDelete(){
+        Log.d("mysql","alterTableForIsNetAndIsDelete ");
+        mSqliteControl.alterTableForIsNetAndIsDelete();
+        Log.d("mysql","alterTableForIsNetAndIsDelete end");
+    }
+
+    public boolean isUpdate(){
+        Log.d("mysql","isUpdate ");
+        boolean value= mSqliteControl.isUpdate();
+        Log.d("mysql","isUpdate end ="+value);
+        return value;
+    }
+
+    public void clearAllDelete() {
+        Log.d("mysql","clearAllDelete ");
+        mSqliteControl.clearAllDelete();
+        Log.d("mysql","clearAllDelete end");
+    }
+
+    public void inSertDate( String str){
+        Log.d("mysql","inSertDate str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.inSertDate(null,date);
+        Log.d("mysql","inSertDate end");
+    }
+
+    public void changeGoodType(String str)
+    {
+        Log.d("mysql","changeGoodType str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.changeGoodType(date);
+        Log.d("mysql","changeGoodType end");
+    }
+
+    public void changeGoodSql(String str,String oldstr)
+    {
+        Log.d("mysql","changeGoodSql str="+str);
+        long old = Long.parseLong(oldstr);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.changeGoodSql(date,old);
+        Log.d("mysql","changeGoodSql end");
+    }
+
+    public void updateIdAndType(String str)
+    {
+        Log.d("mysql","updateIdAndType str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.updateIdAndType(date);
+        Log.d("mysql","updateIdAndType end");
+    }
+
+    public void deleteIdAndType(String str)
+    {
+        Log.d("mysql","deleteIdAndType str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.deleteIdAndType(date);
+        Log.d("mysql","deleteIdAndType end");
+    }
+
+    public void deleteGood(String str)
+    {
+        Log.d("mysql","deleteGood str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.deleteGood(date);
+        Log.d("mysql","deleteGood end");
+
+    }
+
+    public void deleteLuiHui()
+    {
+        Log.d("mysql","deleteLuiHui ");
+        mSqliteControl.deleteLuiHui();
+        Log.d("mysql","deleteLuiHui end");
+    }
+
+    public void UpdateZhuangbeiInto(String str)
+    {
+        Log.d("mysql","UpdateZhuangbeiInto str="+str);
+        SQLDate date = stringToSqlDate(str);
+        mSqliteControl.UpdateZhuangbeiInto(date);
+        Log.d("mysql","UpdateZhuangbeiInto end");
+    }
+
+    public void updateEndNet(String str){
+        Log.d("mysql","updateEndNet str="+str);
+        List<SQLDate> list = stringToSqlDateList(str);
+        mSqliteControl.updateEndNet(list);
+        Log.d("mysql","updateEndNet end");
+    }
+
+    public void deleteCleanNet()
+    {
+        Log.d("mysql","deleteCleanNet ");
+        mSqliteControl.deleteCleanNet();
+        Log.d("mysql","deleteCleanNet end");
+    }
+
+    public void removeDeleteDate(){
+        Log.d("mysql","removeDeleteDate ");
+        mSqliteControl.removeDeleteDate();
+        Log.d("mysql","removeDeleteDate end");
+    }
+
+    public String getNetDate(){
+        Log.d("mysql","getNetDate ");
+        List<SQLDate> list =  mSqliteControl.getNetDate();
+        String value =  sqlDateListToString(list);
+        Log.d("mysql","removeDeleteDate end ="+value);
+        return value;
+    }
+
+    public String getAll(){
+        Log.d("mysql","getAll ");
+        List<SQLDate> list =  mSqliteControl.getAll();
+        String value = sqlDateListToString(list);
+        Log.d("mysql","getAll end ="+value);
+        return value;
+    }
+
+    public void delectAll(String tableName){
+        Log.d("mysql","delectAll  tableName="+tableName);
+        mSqliteControl.delectAll(tableName);
+        Log.d("mysql","delectAll end");
+    }
+
+    public void saveLocal(String str){
+        Log.d("mysql","saveLocal  str="+str);
+        List<SQLDate> list = stringToSqlDateList(str);
+        mSqliteControl.saveLocal(list);
+        Log.d("mysql","saveLocal end");
+    }
+
+    private SQLDate stringToSqlDate(String str){
+        try {
+            JSONObject jb =new JSONObject(str);
+            SQLDate date = new SQLDate();
+            date.type = jb.getLong("type");
+            date.id = jb.getLong("id");
+            date.goodId = jb.getLong("goodId");
+            date.goodType = jb.getLong("goodType");
+            date.isClean = jb.getLong("isClean");
+            date.extan = jb.getString("extan");
+            date.isDelete = jb.getLong("isDelete");
+            date.isNet = jb.getLong("isNet");
+            return  date;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    private List<SQLDate> stringToSqlDateList(String str){
+        try {
+            ArrayList<SQLDate> list = new ArrayList<>();
+            JSONArray array = new JSONArray(str);
+            for(int i = 0 ; i< array.length() ; i++){
+                JSONObject jb = array.getJSONObject(i);
+                SQLDate date = new SQLDate();
+                date.type = jb.getLong("type");
+                date.id = jb.getLong("id");
+                date.goodId = jb.getLong("goodId");
+                date.goodType = jb.getLong("goodType");
+                date.isClean = jb.getLong("isClean");
+                date.extan = jb.getString("extan");
+                date.isDelete = jb.getLong("isDelete");
+                date.isNet = jb.getLong("isNet");
+                list.add(date);
+            }
+            return list;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+    private String sqlDateListToString(List<SQLDate> list){
+        JSONArray array = new JSONArray();
+        try {
+            for(SQLDate date : list){
+                JSONObject jb =new JSONObject();
+                jb.put("type", date.type);
+                jb.put("id", date.id);
+                jb.put("goodId", date.goodId);
+                jb.put("goodtype", date.goodType);
+                jb.put("isclean", date.isClean);
+                jb.put("extra", date.extan);
+                jb.put("isDelete", date.isDelete);
+                jb.put("isNet", date.isNet);
+                array.put(jb);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return array.toString();
+    }
+
     public String showTaptap(String str){
-  /*      Log.d("jackzhng","playAd===========================");
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setData(Uri.parse("taptap://taptap.com/app?app_id=150760&source=outer"));
-        if (intent != null && intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }else{
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse("http://d.taptap.com/latest?app_id=150760"));
-            startActivity(intent);
-        }*/
         return "";
     }
 
--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/sqlite/SqliteControl.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/sqlite/SqliteControl.java
@@ -2,10 +2,13 @@ package com.jackzheng.ourgame.demonadshowlib.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.ArrayMap;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SqliteControl extends SQLiteOpenHelper{
@@ -16,14 +19,23 @@ public class SqliteControl extends SQLiteOpenHelper{
     private static final String TAG = "JackSqlite";
 
     public SqliteControl(Context context, String name, SQLiteDatabase.CursorFactory factory,
-                         int version) {
+                         int version,String tableName) {
         super(context, name, factory, version);
+        this.tableName = tableName;
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "create table stu_table(id int,sname varchar(20),sage int,ssex varchar(10))";
+        String sql = "CREATE TABLE " + tableName + "(" +
+                "TYPE          INT ," +
+                "ID            INT ," +
+                "EXTAN         TEXT ," +
+                "GOODID        INT ," +
+                "GOODTYPE      INT ," +
+                "ISCLENAN      INT ," +
+                "ISNET      INT ," +
+                "ISDELETE      INT )";
         Log.i(TAG, "create Database------------->");
         db.execSQL(sql);
     }
@@ -31,6 +43,40 @@ public class SqliteControl extends SQLiteOpenHelper{
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.i(TAG, "update Database------------->");
+    }
+
+    public long getLevel(){
+        SQLiteDatabase db =getReadableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{"EXTAN"}, "ID=? and AND TYPE=? AND ISDELETE=1",  new String[]{"1","1"}, null, null, null);
+        long level = -100;
+        while(cursor.moveToNext()){
+            level= Long.parseLong( cursor.getString(cursor.getColumnIndex("EXTAN")) );
+        }
+        db.close();
+        return level;
+    }
+
+    public long getPlayVocation(){
+        SQLiteDatabase db =getReadableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{"EXTAN"}, "ID=? and AND TYPE=?  AND ISDELETE=1",  new String[]{"22","1"}, null, null, null);
+        long level = -100;
+        while(cursor.moveToNext()){
+            level= Long.parseLong( cursor.getString(cursor.getColumnIndex("EXTAN")) );
+        }
+        db.close();
+        return level;
+    }
+
+    public void deleteGuide(long id)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("ISDELETE", 2);
+        String whereClause="TYPE=8 and id=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(id)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
     }
 
 
@@ -42,9 +88,49 @@ public class SqliteControl extends SQLiteOpenHelper{
         cv.put("ISNET", 1);
         String whereClause="Type=? and id=? and isdelete=1";
         String [] whereArgs = {String.valueOf(date.type),String.valueOf(date.id)};
-        db.update(sqlName, cv, whereClause, whereArgs);
+        db.update(tableName, cv, whereClause, whereArgs);
         db.close();
     }
+
+    public void alterTableForIsNetAndIsDelete(){
+        try {
+            SQLiteDatabase db = getWritableDatabase();
+            String common =  "alter table " + tableName + " add ISDELETE int default 1";
+            db.execSQL(common);
+
+            common =  "alter table " + tableName + " add ISDELETE int default 1";
+            db.execSQL(common);
+
+            db.close();
+
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isUpdate(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{"EXTAN"}, "ID=? and AND TYPE=?",  new String[]{"18","1"}, null, null, null);
+        long count =0;
+        while(cursor.moveToNext()){
+            count++;
+        }
+        if (count > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void clearAllDelete() {
+        SQLiteDatabase db= getReadableDatabase();
+
+        String whereClauses = "ISDELETE=?";
+        String [] whereArgs = {String.valueOf(2)};
+        db.delete(tableName, whereClauses, whereArgs);
+        db.close();
+    }
+
 
     public void inSertDate(SQLiteDatabase db ,SQLDate date){
         boolean isClocse = false;
@@ -61,36 +147,161 @@ public class SqliteControl extends SQLiteOpenHelper{
         cv.put("ISCLENAN", date.isClean);
         cv.put("ISNET", date.isNet);
         cv.put("ISDELETE", date.isDelete);
-        db.insert(sqlName, null, cv);
+        db.insert(tableName, null, cv);
         if(isClocse){
             db.close();
         }
     }
 
-    public void deleteDateBySqldate(SQLiteDatabase db ,SQLDate date){
-        boolean isClocse = false;
-        if(db == null){
-            isClocse = true;
-            db = getReadableDatabase();
-        }
-        String whereClauses = "TYPE=? and ID=? and EXTAN='?' and GOODID=? and GOODTYPE=? and ISCLENAN=? and ISNET=? and ISDELETE=?";
-        String [] whereArgs = {String.valueOf(date.type),String.valueOf(date.id),date.extan,String.valueOf(date.goodId)
-                ,String.valueOf(date.goodType),String.valueOf(date.isClean),String.valueOf(date.isNet),String.valueOf(date.isDelete),};
-        db.delete(sqlName, whereClauses, whereArgs);
-        if(isClocse){
-            db.close();
-        }
+    public void changeGoodType(SQLDate date)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("GOODTYPE", date.type);
+        String whereClause="GOODID=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(date.goodId)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
     }
 
-    public void deleteDateEndNet(List<SQLDate> list){
+    public void changeGoodSql(SQLDate date,long old)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("GOODID", date.goodId);
+        String whereClause="GOODID=? and TYPE=? and id=? and TYPE=1";
+        String [] whereArgs = {String.valueOf(old),String.valueOf(date.type),String.valueOf(date.id)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
+    }
+
+    public void updateIdAndType(SQLDate date)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("EXTAN", date.extan);
+        String whereClause="TYPE=? and id=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(date.type),String.valueOf(date.id)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
+    }
+    public void deleteIdAndType(SQLDate date)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("ISDELETE", 2);
+        String whereClause="TYPE=? and id=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(date.type),String.valueOf(date.id)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
+    }
+
+    public void deleteGood(SQLDate date)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("ISDELETE", 2);
+        String whereClause="GOODID=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(date.goodId)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
+
+    }
+    public void deleteLuiHui()
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("ISNET", 1);
+        cv.put("ISDELETE", 2);
+        String whereClause="ISCLENAN=1 and ISDELETE=1";
+        db.update(tableName, cv, whereClause, null);
+        db.close();
+    }
+
+    public void UpdateZhuangbeiInto(SQLDate date)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("EXTAN",date.extan);
+        cv.put("ISNET", 1);
+        String whereClause="GOODID=? and ISDELETE=1";
+        String [] whereArgs = {String.valueOf(date.goodId)};
+        db.update(tableName, cv, whereClause, whereArgs);
+        db.close();
+    }
+
+    public void updateEndNet(List<SQLDate> list){
         SQLiteDatabase db =getWritableDatabase();
         for(SQLDate index  : list){
-            deleteDateBySqldate(db,index);
+            ContentValues cv = new ContentValues();
+            cv.put("ISNET", 2);
+            String whereClause="TYPE=? and ID=? and EXTAN=? and GOODID=? and GOODTYPE=? and ISCLENAN=? and ISNET=? and ISDELETE=?";
+            String [] whereArgs = {String.valueOf(index.type),String.valueOf(index.id),index.extan,String.valueOf(index.goodId)
+                    ,String.valueOf(index.goodType),String.valueOf(index.isClean),String.valueOf(index.isNet),String.valueOf(index.isDelete),};
+            db.update(tableName, cv, whereClause, whereArgs);
         }
+        db.close();
     }
 
-    public void getAll(){
+    public void deleteCleanNet()
+    {
+        SQLiteDatabase db= getReadableDatabase();
 
+        String whereClauses = "TYPE=11";
+        db.delete(tableName, whereClauses, null);
+        db.close();
+    }
+
+    public void removeDeleteDate(){
+        SQLiteDatabase db= getReadableDatabase();
+
+        String whereClauses = "ISNET=2 AND ISDELETE=2";
+        db.delete(tableName, whereClauses, null);
+        db.close();
+    }
+
+    public List<SQLDate> getNetDate(){
+        ArrayList<com.jackzheng.ourgame.demonadshowlib.sqlite.SQLDate> list = new ArrayList<>();
+        SQLiteDatabase db =getWritableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{"TYPE","ID","EXTAN","GOODID","GOODTYPE","ISCLENAN","ISNET","ISDELETE",}, "ISNET=2 AND ISDELETE=2", null, null, null, null);
+
+        while(cursor.moveToNext()){
+            list.add(cursorToSqlDate(cursor));
+        }
+        db.close();
+        return  list;
+
+    }
+
+    private SQLDate cursorToSqlDate(Cursor cursor){
+        SQLDate index = new SQLDate();
+        index.type= cursor.getLong(cursor.getColumnIndex("TYPE"));
+        index.id= cursor.getLong(cursor.getColumnIndex("ID"));
+        index.extan= cursor.getString(cursor.getColumnIndex("EXTAN"));
+        index.goodId= cursor.getLong(cursor.getColumnIndex("GOODID"));
+        index.goodType= cursor.getLong(cursor.getColumnIndex("GOODTYPE"));
+        index.isClean= cursor.getLong(cursor.getColumnIndex("ISCLENAN"));
+        index.isNet= cursor.getLong(cursor.getColumnIndex("ISNET"));
+        index.isDelete= cursor.getLong(cursor.getColumnIndex("ISDELETE"));
+        return index;
+
+    }
+
+    public List<SQLDate> getAll(){
+        ArrayList<SQLDate> list = new ArrayList<>();
+        SQLiteDatabase db =getWritableDatabase();
+        Cursor cursor = db.query(tableName, new String[]{"TYPE","ID","EXTAN","GOODID","GOODTYPE","ISCLENAN","ISNET","ISDELETE",}, null, null, null, null, null);
+
+        while(cursor.moveToNext()){
+            list.add(cursorToSqlDate(cursor));
+        }
+        db.close();
+        return  list;
     }
 
     public void delectAll(String tableName){
@@ -100,7 +311,7 @@ public class SqliteControl extends SQLiteOpenHelper{
     }
 
     public void saveLocal(List<SQLDate> list){
-        delectAll(sqlName);
+        delectAll(tableName);
         SQLiteDatabase db =getWritableDatabase();
         for(SQLDate index  : list){
             if(index.type == 10){
@@ -110,9 +321,4 @@ public class SqliteControl extends SQLiteOpenHelper{
         }
         db.close();
     }
-
-    public void deleteNetData(){
-
-    }
-
 }
