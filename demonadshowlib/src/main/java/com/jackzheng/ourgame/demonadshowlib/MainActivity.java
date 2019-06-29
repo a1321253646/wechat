4486257e847281b8bae683195d55532293151b0c
1321253646@qq.com <1321253646@qq.com>
2019-06-29 10:56:25 +0800

    假如vivo的初始化

--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainActivity.java
@@ -1,6 +1,11 @@
 package com.jackzheng.ourgame.demonadshowlib;
 
 
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+import com.vivo.unionsdk.open.VivoExitCallback;
+import com.vivo.unionsdk.open.VivoUnionSDK;
+
 public class MainActivity extends MainActivityBase {
 
 
@@ -27,5 +32,22 @@ public class MainActivity extends MainActivityBase {
     @Override
     public void startShowBannerDeal() {
 
+    }
+
+    public String exitGame(String str){
+        VivoUnionSDK.exit(this, new VivoExitCallback() {
+            @Override
+            public void onExitCancel() {
+                //取消退出
+            }
+
+            @Override
+            public void onExitConfirm() {
+                UnityPlayer.UnitySendMessage("Main Camera", "exitGame", "param");
+                finish();
+            }
+        });
+
+        return "";
     }
 }
--- demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
+++ demonadshowlib/src/main/java/com/jackzheng/ourgame/demonadshowlib/MainApplication.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
+import com.vivo.unionsdk.open.VivoUnionSDK;
 
 import org.apache.commons.logging.Log;
 
@@ -14,6 +15,8 @@ public class MainApplication extends Application {
 
     public static String[] mAppKeyUmeng = {"5cd62f530cafb2cc4c0007df","5cd62f633fc1957973000f46","5cd62f783fc195f2680002ef","5cd62f884ca3570403000919","5cd62f9b570df34c47000fde",
             "5cd62fbc0cafb254a00004ab","5cd62fce570df31d670001d5","5cd62fdd3fc19537dd000660","5cd62fe73fc195f268000303"};
+
+    public static String appKey = "670bd90cdccb094ae6a4d2753f6396c5";
 
     @Override
     public void onCreate() {
@@ -40,6 +43,8 @@ public class MainApplication extends Application {
         UMConfigure.setLogEnabled(true);
         UMConfigure.init(this, mAppKeyUmeng[Integer.parseInt(name)], "Umeng", UMConfigure.DEVICE_TYPE_PHONE,null);
 
+
+        VivoUnionSDK.initSdk(this, appKey, false);
     }
 
 }
